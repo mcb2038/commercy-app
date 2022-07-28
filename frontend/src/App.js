156d70602096a0ae8585558eb13cc09323e83a53
Michael Baron <mcb2038@gmail.com>
2022-07-28 15:12:14 -0400

    initial file structure

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Commercy</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
